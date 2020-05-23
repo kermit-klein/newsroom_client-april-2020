5743d0220087333f18cdcbb91ec38af95748a4d5
designerofthing <designerofthings@gmail.com>
2020-05-23 11:21:22 +0200

    adds test for single article page ads

--- cypress/integration/visitorCanSeeAds.feature.js
+++ cypress/integration/visitorCanSeeAds.feature.js
@@ -26,4 +26,17 @@ describe("Visitor can see ads", () => {
       );
     cy.get("#ad-2").click();
   });
+
+  it("on single article page ad1", () => {
+    cy.get("#article-1").within(() => {
+      cy.get("#article-title").click()});
+    cy.get("#ad-1").should("be.visible");
+    cy.get("#ad-1")
+      .should("have.attr", "href")
+      .and(
+        "include",
+        "https://www.mercedes-benz.com/en/"
+      );
+    cy.get("#ad-1").click();
+  });
 });
