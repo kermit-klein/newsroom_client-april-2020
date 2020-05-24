d378056fab1f970afa0504f566bf278e33b3b3ff
allezz <aerbay@gmail.com>
2020-05-24 16:14:33 +0200

    Add Heroku baseurl

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
-import './css/Header.css'
+import "./css/Header.css";
 
-
-
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://newsroom-april2020-api.herokuapp.com/api";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-document.getElementById("root"));
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
