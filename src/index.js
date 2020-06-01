cf597ea5c36132ca397eadd18d8640810bd1cc64
allezz <aerbay@gmail.com>
2020-06-01 21:00:54 +0200

    Add environment variables

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
-import { StripeProvider } from 'react-stripe-elements'
+import { StripeProvider } from "react-stripe-elements";
 import "./css/index.css";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+if (process.env.NODE_ENV === "production") {
+  axios.defaults.baseURL = process.env.REACT_APP_HEROKUURL;
+} else if (process.env.NODE_ENV === "development") {
+  axios.defaults.baseURL = process.env.REACT_APP_LOCALURL;
+}
 
 ReactDOM.render(
   <StripeProvider apiKey="pk_test_21nBNjeqdyB1Mzm2VjDPQprF00kyEKYZSK">
